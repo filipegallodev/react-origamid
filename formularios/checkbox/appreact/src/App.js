b847c05004de760fb19c6d02ed5fdc33cc6bcae6
Filipe <fgallo_dev@hotmail.com>
2022-11-23 08:37:17 -0300

    Checkbox 1

---
+++ formularios/checkbox/appreact/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const App = () => {
+  const [cores, setCores] = React.useState(["vermelho"]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  return (
+    <form>
+      <label>
+        <input
+          type="checkbox"
+          value="azul"
+          checked={cores.includes("azul")}
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={cores.includes("vermelho")}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
+    </form>
+  );
+};
+
+export default App;
