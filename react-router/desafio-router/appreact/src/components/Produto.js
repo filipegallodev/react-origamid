cee7ba48f6af4a503ee167da897b942da2777d86
Filipe <fgallo_dev@hotmail.com>
2022-12-12 14:23:10 -0300

    Desafio Router 2

---
+++ react-router/desafio-router/appreact/src/components/Produto.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Head from "./Head";
+
+import style from "./Produto.module.css";
+
+const Produto = () => {
+  const { id } = useParams();
+
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    async function fetchProduto(url) {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const data = await response.json();
+        setProduto(data);
+      } catch (erro) {
+        setError("Um erro ocorreu");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, []);
+
+  if (loading) return <div>Carregando...</div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
+  return (
+    <section className={style.produto + " animeLeft"}>
+      <Head
+        title={`Ranek | ${produto.nome}`}
+        description={`Ranek | Esse é um produto: ${produto.nome}`}
+      />
+      {produto.fotos.map((foto) => (
+        <img key={foto.src} src={foto.src} alt={foto.titulo} />
+      ))}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={style.preco}>R$ {produto.preco}</span>
+        <p className={style.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Produto;
--- react-router/desafio-router/appreact/src/components/Produtos.js
+++ react-router/desafio-router/appreact/src/components/Produtos.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Head from "./Head";
 
 import style from "./Produtos.module.css";
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://ranekapi.origamid.dev/json/api/produto")
+      .then((res) => res.json())
+      .then((data) => setProdutos(data));
+  }, []);
+
+  if (produtos === null) return null;
   return (
-    <div className={style.produtos}>
-      <h1>Produtos</h1>
-    </div>
+    <section className={style.produtos + " animeLeft"}>
+      <Head title="Ranek | Produtos" description={`Descrição do site Ranek`} />
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={style.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
   );
 };
 
