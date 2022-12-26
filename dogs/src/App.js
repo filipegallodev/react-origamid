814f9ed87335b18b389fb064523b94b181e9530a
Filipe <filipegallodev@hotmail.com>
2022-12-26 09:20:17 -0300

    Estrutura Inicial

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
 import "./App.css";
 
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
