9d270312437c772f094cd68a95203e9c00846ef9
Filipe <filipegallodev@hotmail.com>
2022-12-27 13:46:51 -0300

    Protected Route

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -3,9 +3,11 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import { UserStorage } from "./UserContext";
 
 import "./App.css";
-import { UserStorage } from "./UserContext";
 
 function App() {
   return (
@@ -15,7 +17,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
