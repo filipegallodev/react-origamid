26ee25d2a35b3aca0b41ee962c11774fc50a63c2
Filipe <filipegallodev@hotmail.com>
2022-12-29 08:56:40 -0300

    Foto Post

--- dogs/src/Hooks/useForm.js
+++ dogs/src/Hooks/useForm.js
@@ -4,12 +4,16 @@ const types = {
   email: {
     regex:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: "Preencha um email válido",
+    message: "Preencha um email válido.",
   },
   password: {
     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     message:
       "A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 número. Com no mínimo 8 caracteres.",
+  },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize números apenas.",
   },
 };
 
