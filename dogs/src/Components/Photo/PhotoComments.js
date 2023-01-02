d57599c9ed976dde8db0a31acbb0464949c8e0f7
Filipe <filipegallodev@hotmail.com>
2023-01-02 09:21:15 -0300

    Comentário Post

--- dogs/src/Components/Photo/PhotoComments.js
+++ dogs/src/Components/Photo/PhotoComments.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
 
-const PhotoComments = () => {
-  return <div></div>;
+import styles from "./PhotoComments.module.css";
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map(({ comment_ID, comment_author, comment_content }) => (
+          <li key={comment_ID}>
+            <b>{comment_author}: </b>
+            <span>{comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
